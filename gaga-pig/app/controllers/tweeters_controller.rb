be678bada4d4b5178a666415d9a431515ffdc22c
pedrosnk <pedrosnk@0cdcf803-4e8e-d692-614d-d13140bc2177>
2010-09-17 02:50:28 +0000

    commit legal

--- gaga-pig/app/controllers/tweeters_controller.rb
+++ gaga-pig/app/controllers/tweeters_controller.rb
@@ -1,7 +1,19 @@
 class TweetersController < ApplicationController
   def index
-    @tweeters = Tweeters.find(:first)
+    @tweeters = Tweeters.find(:first, :conditions => { :screen_name => params[:tweet_url] })
+  end
 
+  def status
+    @tweeters = Tweeters.find(:all, :conditions => { :screen_name => params[:tweet_url] })
+    @tweet_status = Array.new
+    @tweeters.each do |t|
+      Statuses.find(:all ).each do |s|
+        if (s.user == t._id)
+          @tweet_status << s
+        end
+      end
+    end
+    #@tweet_status = Statuses.find(:first)
   end
 
 end
